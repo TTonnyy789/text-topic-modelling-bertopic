6d10f1099e9d9d50805568ff4f5f2c3be0754924
TTonnyy789 <109502468+TTonnyy789@users.noreply.github.com>
2023-08-04 10:34:09 +0800

    Updated python script
    Designed a user friendly UI of topic prediction function

--- BERTopic.py
+++ BERTopic.py
@@ -7,7 +7,16 @@ def load_model(model_path):
 
 def predict_topic(model, text, num_of_topics=3):
     similar_topics, similarity = model.find_topics(text, top_n=num_of_topics)
+    for topic in similar_topics:
+        topic_words = model.get_topic(topic)[:3]  # get the top 3 keywords
+        print(f"Topic {topic} top {num_of_topics} keywords: {', '.join([word for word, freq in topic_words])}")
     print(f"The top {num_of_topics} similar topics are {similar_topics}, and the similarities are {np.round(similarity, 2)}")
+
+
+
+# def predict_topic(model, text, num_of_topics=3):
+#     similar_topics, similarity = model.find_topics(text, top_n=num_of_topics)
+#     print(f"The top {num_of_topics} similar topics are {similar_topics}, and the similarities are {np.round(similarity, 2)}")
 
 if __name__ == "__main__":
     model_path = "Analysis/BloomBerg_model" 
